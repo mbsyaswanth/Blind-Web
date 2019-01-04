d502ffedba05515f4eca3b734a6495673926390e
mbsyaswanth <mbs.yaswanth@gmail.com>
2019-01-04 08:31:55 +0530

    changed position of service worker

--- serviceWorker1/pwabuilder-sw-register.js
+++ serviceWorker1/pwabuilder-sw-register.js
@@ -5,8 +5,8 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuider-sw.js', {
-    scope: './'
+  navigator.serviceWorker.register('serviceWorker1/pwabuider-sw.js', {
+    scope: './Blind-Web'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
