d19a135cc10a946723561f28a6391b5cd62ad7e4
mbsyaswanth <mbs.yaswanth@gmail.com>
2019-01-05 06:45:32 +0530

    trying to fix service worker registration

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -5,7 +5,7 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('serviceWorker1/pwabuider-sw.js', {
+  navigator.serviceWorker.register('pwabuider-sw.js', {
     scope: './Blind-Web'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
